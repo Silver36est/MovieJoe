1342e7a15de36989cf9c2cc248ee8bc2f6a605f1
Silver Kusman <silverkusman@gmail.com>
2021-11-21 09:38:39 +0200

    Initial commit

---
+++ src/main/java/com/example/MovieJoe/MovieJoeApplication.java
@@ -0,0 +1,13 @@
+package com.example.MovieJoe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieJoeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieJoeApplication.class, args);
+	}
+
+}
