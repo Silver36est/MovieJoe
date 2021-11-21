3091de98a9e2816a7bfd25c2f40a74c2a6da14ae
Silver Kusman <silverkusman@gmail.com>
2021-11-21 12:44:57 +0200

    Add Movie entity java class

--- src/main/java/com/example/MovieJoe/entities/Movie.java
+++ src/main/java/com/example/MovieJoe/entities/Movie.java
@@ -1,4 +1,28 @@
 package com.example.MovieJoe.entities;
 
+import lombok.*;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+@Entity
+@Table(name = "movies")
 public class Movie {
+    @Id
+    @GeneratedValue
+    private long id;
+    @NotNull(message = "Must provide description")
+    private String title;
+    private String genre;
+    private String releaseYear;
+    private String length;
+    private String description;
 }
