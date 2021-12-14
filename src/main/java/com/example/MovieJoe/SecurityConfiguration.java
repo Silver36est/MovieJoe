d8786ebe7845170f9ab2dae3fddff577f6051ba2
Kristi Ra <kristiratsep01@gmail.com>
2021-12-14 12:32:30 +0200

    updated spring security

--- src/main/java/com/example/MovieJoe/SecurityConfiguration.java
+++ src/main/java/com/example/MovieJoe/SecurityConfiguration.java
@@ -15,6 +15,7 @@ import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import java.util.Arrays;
+import java.util.List;
 
 @EnableWebSecurity
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
@@ -29,18 +30,20 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
+        http = http.cors().and().csrf().disable();
         http
                 .authorizeRequests()
-                    .antMatchers("/", "/register", "/login").permitAll()
-                    .antMatchers("/home").hasAnyRole()
-                  //  .antMatchers( "/user/check").permitAll()
-                    .and()
+                .antMatchers("/", "/register", "/login").permitAll()
+                .antMatchers("/home").authenticated()
+                //  .antMatchers( "/user/check").permitAll()
+                .and()
                 .formLogin()
-                    .loginPage("/login")
-                    .permitAll()
-                    .and()
+                .loginPage("/login")
+                .permitAll()
+                .and()
                 .logout()
-                    .permitAll();
+                .permitAll();
+                //http.csrf().disable();
     }
 
     @Bean
@@ -48,18 +51,17 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
         return new BCryptPasswordEncoder();
     }
 
-    @Bean
+    /*@Bean
     CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("*"));
+        configuration.setAllowedOrigins(List.of("*"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "OPTIONS", "DELETE", "PUT", "PATCH"));
-        configuration.setAllowedHeaders(Arrays.asList("*"));
+        configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);
-        configuration.setMaxAge(1800L);
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", configuration);
         return source;
-    }
+    }*/
 
 
 }
--- src/main/java/com/example/MovieJoe/controllers/UserController.java
+++ src/main/java/com/example/MovieJoe/controllers/UserController.java
@@ -13,7 +13,7 @@ import java.util.List;
 @Controller
 @RequestMapping("/api")
 @ResponseBody
-//@CrossOrigin("http://localhost:4200")
+@CrossOrigin("http://localhost:4200")
 public class UserController {
 
     @Autowired
